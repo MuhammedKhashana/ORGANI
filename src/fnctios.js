4f5843d9e14ea69718f87d311e11d661e11e2df3
Mohamed Khashana <118824280+mokhashana@users.noreply.github.com>
2024-05-10 17:27:05 +0300

    first commit

--- src/fnctios.js
+++ src/fnctios.js
@@ -1,34 +1,3 @@
-// const tasks = []
-
-// function addTask(newTask) {
-//     tasks.push(newTask);
-// }
-
-// function isCompleted({ id, statu }) {
-//     const objIndex = tasks.findIndex(task => task.id == id);
-//     tasks[objIndex].data.isCompleted = statu;
-// }
-
-
-// function editTask({ id, newTitle, newPriority, newDate }) {
-//     const objIndex = tasks.findIndex(task => task.id == id);
-//     tasks[objIndex].data.title = newTitle;
-//     tasks[objIndex].data.priority = newPriority;
-//     tasks[objIndex].data.date = newDate;
-// }
-
-// function deleteTaskX(idToDelete) {
-//     tasks.concat(tasks.filter((task) => task.id !== idToDelete)).splice(tasks.length/2, tasks.length)
-// }
-
-// export {
-//     tasks,
-//     addTask,
-//     editTask,
-//     isCompleted,
-//     deleteTaskX
-// }
-
 const tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
 
 function addTask(newTask) {
