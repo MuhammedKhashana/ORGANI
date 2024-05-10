a4c8cd19d8fd78d28d95d7a333ee737a467ecd42
Mohamed Khashana <118824280+mokhashana@users.noreply.github.com>
2024-05-10 17:25:14 +0300

    first commit

--- src/fnctios.js
+++ src/fnctios.js
@@ -1,30 +1,61 @@
-const tasks = []
+// const tasks = []
+
+// function addTask(newTask) {
+//     tasks.push(newTask);
+// }
+
+// function isCompleted({ id, statu }) {
+//     const objIndex = tasks.findIndex(task => task.id == id);
+//     tasks[objIndex].data.isCompleted = statu;
+// }
+
+
+// function editTask({ id, newTitle, newPriority, newDate }) {
+//     const objIndex = tasks.findIndex(task => task.id == id);
+//     tasks[objIndex].data.title = newTitle;
+//     tasks[objIndex].data.priority = newPriority;
+//     tasks[objIndex].data.date = newDate;
+// }
+
+// function deleteTaskX(idToDelete) {
+//     tasks.concat(tasks.filter((task) => task.id !== idToDelete)).splice(tasks.length/2, tasks.length)
+// }
+
+// export {
+//     tasks,
+//     addTask,
+//     editTask,
+//     isCompleted,
+//     deleteTaskX
+// }
+
+const tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
 
 function addTask(newTask) {
     tasks.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function isCompleted({ id, statu }) {
-    const objIndex = tasks.findIndex(task => task.id == id);
+    const objIndex = tasks.findIndex((task) => task.id == id);
     tasks[objIndex].data.isCompleted = statu;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-
 function editTask({ id, newTitle, newPriority, newDate }) {
-    const objIndex = tasks.findIndex(task => task.id == id);
+    const objIndex = tasks.findIndex((task) => task.id == id);
     tasks[objIndex].data.title = newTitle;
     tasks[objIndex].data.priority = newPriority;
     tasks[objIndex].data.date = newDate;
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function deleteTaskX(idToDelete) {
-    tasks.concat(tasks.filter((task) => task.id !== idToDelete)).splice(tasks.length/2, tasks.length)
+    tasks.splice(
+        tasks.findIndex((task) => task.id == idToDelete),
+        1,
+    );
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-export {
-    tasks,
-    addTask,
-    editTask,
-    isCompleted,
-    deleteTaskX
-}
+export { tasks, addTask, editTask, isCompleted, deleteTaskX };
